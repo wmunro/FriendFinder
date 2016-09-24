2296062d6851de7646851e5a00b7340533039a1d
wmunro <munro.william@yahoo.com>
2016-09-24 16:24:05 -0400

    fixed image box not showing up, tried using .chosen for drop down boxes but later removed it

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friendsobj = [
 	{
 		name:"Jar Jar",
-		photo:"https://i.ytimg.com/vi/PXh47Z_iYlU/maxresdefault.jpg",
+		photo:"https://s-media-cache-ak0.pinimg.com/236x/6a/3d/91/6a3d914da55390d198b3b9d4b9d196dd.jpg",
 		scores:["1","1","1","1","1","1","1","1","1","1"]
 	},
 	{
@@ -12,43 +12,43 @@ var friendsobj = [
 	},
 	{
 		name:"Luke Skywalker",
-		photo:"",
+		photo:"https://67.media.tumblr.com/a9bb7350e5c27f41a91dab633c77e577/tumblr_o5847t98cx1rulc91o1_500.jpg",
 		scores:["3","3","3","3","3","3","3","3","3","3"]
 	},
 	{
-		name:"",
-		photo:"",
+		name:"Lando",
+		photo:"http://nerdreactor.com/wp-content/uploads/2013/03/billie-d-williams-star-wars-500x500_c.jpg",
 		scores:["4","4","4","4","4","4","4","4","4","4"]
 	},
 	{
-		name:"",
-		photo:"",
+		name:"Han Solo",
+		photo:"http://nerdreactor.com/wp-content/uploads/2015/09/han_solo_star_wars_a_new_hope-500x500_c.jpg",
 		scores:["5","5","5","5","5","5","5","5","5","5"]
 	},
 	{
-		name:"",
-		photo:"",
+		name:"Padme-Amidala",
+		photo:"https://s-media-cache-ak0.pinimg.com/564x/d0/32/d4/d032d4326d1c14c6a795c9d26834a9cf.jpg",
 		scores:["1","2","1","2","1","2","1","2","1","2"]
 	},
 	{
-		name:"",
-		photo:"",
+		name:"Aayla Secura",
+		photo:"https://pbs.twimg.com/profile_images/740525348106932224/eawr2Bgz.jpg",
 		scores:["2","1","2","1","2","1","2","1","2","1"]
 	},
 	{
-		name:"",
-		photo:"",
+		name:"Darth Vader",
+		photo:"http://images4.fanpop.com/image/photos/17100000/Darth-Vader-darth-vader-17182523-500-500.jpg",
 		scores:["3","2","1","3","2","1","3","2","1","3"]
 	},
 	{
-		name:"",
-		photo:"",
+		name:"Rey",
+		photo:"http://nerdreactor.com/wp-content/uploads/2016/03/daisy-ridley-star-wars-500x500_c.jpg",
 		scores:["1","2","3","1","2","3","1","2","3","1"]
 	},
 	{
-		name:"",
-		photo:"",
-		scores:["1","1","2","2","3","3","4","4","5","5"]
+		name:"Yoda",
+		photo:"https://c2.staticflickr.com/4/3533/3215653831_657ea0b3be.jpg",
+		scores:["5","5","5","5","5","5","5","5","5","5"]
 	},
 	
 ];
